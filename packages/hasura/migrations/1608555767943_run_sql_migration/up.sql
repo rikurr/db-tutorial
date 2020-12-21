aee83c1aa9aaa0154f4d6c38640b96b16a5fedff
oya riku <riku03290@gmail.com>
2020-12-21 22:06:32 +0900

    Add:の使用　

---
+++ packages/hasura/migrations/1608555767943_run_sql_migration/up.sql
@@ -0,0 +1,2 @@
+insert into shohinins 
+values ('0006', 'フォーク', 'キッチン用品', 500, null, '2009-09-20');
---
+++ packages/hasura/migrations/1608555842337_run_sql_migration/up.sql
@@ -0,0 +1,2 @@
+insert into shohinins 
+values ('0007', 'おろしがね', 'キッチン用品', default, 790, '2009-04-28');
