b4bbb613dae7de63da81171d6eae7569dd7550d2
oya riku <riku03290@gmail.com>
2020-12-27 22:41:32 +0900

    Add:viewに対してinsertの実行

---
+++ packages/hasura/migrations/1609076433331_run_sql_migration/up.sql
@@ -0,0 +1,4 @@
+CREATE OR REPLACE RULE insert_rule AS ON INSERT
+TO ShohinJim DO INSTEAD
+INSERT INTO Shohin VALUES (
+new.shohin_id, new.shohin_mei, new.shohin_bunrui, new.hanbai_tanka, new.shiire_tanka, new.torokubi);
